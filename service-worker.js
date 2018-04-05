b54a82eac3fb366b10fffdeaee561fde2bbd94c2
SanderSalamander <sander.salamander.larsen@gmail.com>
2018-04-05 13:48:02 +0200

    Added untracked files to commit :high_heel:

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
