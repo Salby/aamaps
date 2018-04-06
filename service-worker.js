ab5826f535f056583ab2e26482082e5ee72ac454
SanderSalamander <sander.salamander.larsen@gmail.com>
2018-04-06 11:59:17 +0200

    :|

--- service-worker.js
+++ service-worker.js
@@ -16,18 +16,18 @@ self.addEventListener('install', function(event) {
     })
   )
 });
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
-      );
-    })
-  );
-});
+// self.addEventListener('activate', function(event) {
+//   event.waitUntil(
+//     caches.keys().then(function(cacheNames) {
+//       return Promise.all(
+//         cacheNames.filter(function(cacheName) {
+//         }).map(function(cacheName) {
+//           return caches.delete(cacheName);
+//         })
+//       );
+//     })
+//   );
+// });
 // self.addEventListener('fetch', function(event) {
 //   event.respondWith(
 //     caches.match(event.request).then(function(response) {
