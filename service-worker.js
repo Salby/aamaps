8dacad784b71f97d43e0d6e706c716aec2dd17dc
SanderSalamander <sander.salamander.larsen@gmail.com>
2018-04-06 11:57:31 +0200

    :)))

--- service-worker.js
+++ service-worker.js
@@ -28,16 +28,16 @@ self.addEventListener('activate', function(event) {
     })
   );
 });
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    }).catch(function() {
-      return caches.match('index.html');
-    })
-  );
-});
 // self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request).then(function(response) {
+//       return response || fetch(event.request);
+//     }).catch(function() {
+//       return caches.match('index.html');
+//     })
+//   );
+// });
+// // self.addEventListener('fetch', function(event) {
 //   event.responsWith(
 //     caches.open(cacheName).then(function(cache) {
 //       return cache.match(event.request).then(function (response) {
